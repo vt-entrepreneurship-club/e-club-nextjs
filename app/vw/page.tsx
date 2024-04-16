21adf42c088179001c6e4e3160d670fd3cf7425e
Ebenezer Zergabachew <eben.zergaw@gmail.com>
2024-04-16 18:52:39 -0400

    time fix

--- app/vw/page.tsx
+++ app/vw/page.tsx
@@ -104,7 +104,7 @@ function VentureWeekend() {
 
             <div className="border-b border-2 border-black"></div>
 
-            <h3 className="text-2xl text-black font-bold mx-auto w-fit mt-8">Pitch Competition - 12pm</h3>
+            <h3 className="text-2xl text-black font-bold mx-auto w-fit mt-8">Pitch Competition - 2pm</h3>
             <p className="text-lg mb-8 text-black mx-auto lg:w-1/2 px-2 text-center mt-4">
               Pitch your ideas to a panel of judges with the chance to win
             </p>
