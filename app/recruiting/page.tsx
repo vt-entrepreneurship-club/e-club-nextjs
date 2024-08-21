f37e498b5fc2bb8211e1c0b4c0728c86553f5b1b
Ebenezer Zergabachew <eben.zergaw@gmail.com>
2024-08-21 11:03:05 -0400

    recruiting page

---
+++ app/recruiting/page.tsx
@@ -0,0 +1,128 @@
+import React from "react";
+import aws from '../assets/company logos/aws.png';
+import bofa from '../assets/company logos/bofa.png';
+import linkedin from '../assets/company logos/linkedin.png';
+import cap1 from '../assets/company logos/cap1.png';
+import kogi from '../assets/company logos/kogi.png';
+import gartner from '../assets/company logos/gartner.png';
+import Image from "next/image";
+
+export const metadata = {
+  title: "Join Our Team",
+  description: "Recruitment page for the Entrepreneurship Club at Virginia Tech.",
+};
+
+const companyLogos = [
+    cap1,
+    kogi,
+    linkedin,
+    aws,
+    bofa,
+    gartner
+]
+
+const roles = [
+  {
+    title: "Digital Media Coordinator",
+    description: `The Digital Media Coordinator is a vital part of our Marketing/Communications team, playing a crucial role in enhancing our online presence and fostering community engagement. This position offers flexible working hours, with a requirement to attend 1-2 meetings per week. As a member of the E-Club leadership team, you will have numerous opportunities for enrichment, allowing you to lead or contribute to various projects. Our structured hierarchy supports upward mobility, offering clear paths for career growth. If you want to join a small, kick-ass team who are trying to build up the next generation of Entrepreneurs, then read on.`,
+    responsibilities: [
+      "Capture high-quality photos and videos at events hosted by E-Club.",
+      "Create and produce engaging short-form and long-form content for our social media platforms.",
+      "Manage, fully or partially, our social media accounts to strengthen our online brand presence.",
+      "Collaborate with other leadership team members to develop and execute exciting initiatives for our members.",
+    ],
+  },
+  {
+    title: "Project Manager",
+    description: `The Project Manager is a pivotal member of our organization, responsible for ensuring that our initiatives run smoothly and successfully. This role is crucial to driving forward our mission to bring entrepreneurship to students. However, it is not a rigidly defined position—it offers significant flexibility and requires a self-starter who can make a meaningful impact regardless of the task at hand.`,
+    responsibilities: [
+      "Attend all weekly meetings (1-2) and E-Club hosted events to ensure seamless execution and coordination.",
+      "Collaborate with and support other teams in achieving their goals and completing tasks.",
+      "Assist in the logistics and planning of events, ensuring all details are managed effectively.",
+      "Address and resolve any loose ends or challenges that arise during projects.",
+      "Strengthen existing partnerships and actively seek new collaborations that align with our mission.",
+      "Innovate and propose new initiatives to foster entrepreneurship among students.",
+    ],
+  },
+  {
+    title: "Design and Branding Lead",
+    description: `The Design and Branding Lead is a creative visionary within our organization, responsible for shaping and elevating our brand's visual identity. This role requires a strong background in graphic design and a passion for art and illustration.`,
+    responsibilities: [
+      "Utilize Photoshop, Figma, and Illustrator to create high-quality designs for various platforms.",
+      "Lead the design and development of the organization's website.",
+      "Design digital assets for social media posts, ensuring consistency with our branding and messaging.",
+      "Create unique and eye-catching designs for physical products, including stickers and shirts.",
+      "Collaborate with the marketing and communications team to align design projects with broader organizational goals.",
+      "Continuously innovate and refine our visual branding to keep it fresh, relevant, and impactful.",
+    ],
+  },
+  {
+    title: "Data and Technology Lead",
+    description: `The Data and Technology Lead is a key technical role within our organization, responsible for driving innovation through web development and data analysis.`,
+    responsibilities: [
+      "Develop and maintain the organization's website using NextJS, ReactJS, and Tailwind CSS.",
+      "Analyze data to extract actionable insights that support the organization's goals and objectives.",
+      "Collaborate with other teams to integrate data-driven solutions into broader projects.",
+      "Continuously monitor and optimize the performance of our digital platforms.",
+      "Stay current with emerging web technologies and data analysis tools.",
+    ],
+  },
+];
+
+const commonRequirements = [
+  "Attend weekly meetings and participate in discussions.",
+  "Commit to working 4-10 hours outside of meetings.",
+  "Be present at E-Club hosted events.",
+  "Demonstrate creativity, strong organizational skills, and the ability to take initiative.",
+];
+
+function RecruitmentPage() {
+  return (
+    <div className="container mx-auto p-6">
+      <h1 className="text-4xl text-primary mb-10 font-bold mx-auto w-fit">Join Our Team</h1>
+
+      <div className="p-4 lg:px-20 border border-red-400 rounded-lg mb-10 w-fit mx-auto">
+        <div className="w-fit mx-auto text-red-400 font-medium text-xl text-center">
+            Applications open <span className="font-bold">Wednesday September 4th</span>
+        </div>
+      </div>
+
+      <div className="text-2xl text-secondary font-semibold mx-auto w-fit text-center">Our leadership members have 100% job placement.</div>
+
+      <div className="mt-10 lg:mt-0 flex flex-col lg:flex-row items-center lg:justify-between w-fit mx-auto ">
+        {
+            companyLogos.map((logo, index) => (
+                <Image key={index} src={logo} alt="" className="w-40 mx-4 lg:mt-0 mt-4" />
+            ))
+        }
+      </div>
+
+
+      <div className="grid gap-10 lg:grid-cols-2 mt-10">
+        {roles.map((role, index) => (
+          <div key={index} className="bg-base-100 p-6 rounded-lg shadow-lg border border-cyan-300">
+            <h2 className="text-2xl text-secondary mb-4">{role.title}</h2>
+            <p className="mb-4">{role.description}</p>
+            <h3 className="text-xl text-primary mb-2">Key Responsibilities:</h3>
+            <ul className="list-disc list-inside mb-4">
+              {role.responsibilities.map((responsibility, idx) => (
+                <li key={idx}>{responsibility}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+
+      <div className="bg-base-100 p-6 rounded-lg shadow-lg mt-10">
+        <h3 className="text-2xl text-secondary mb-4">Requirements</h3>
+        <ul className="list-disc list-inside">
+          {commonRequirements.map((requirement, idx) => (
+            <li key={idx}>{requirement}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default RecruitmentPage;
--- app/startup-sprint/2024/page.tsx
+++ app/startup-sprint/2024/page.tsx
@@ -2,10 +2,21 @@ import React from "react";
 import CountdownTimer from "../../components/CountdownTimer";
 import StartupSprintLogo from "./startup sprint logo.png";
 import Image from "next/image";
+import { FaMoneyBill } from "react-icons/fa";
+import {
+    BsFillExclamationOctagonFill,
+    BsPeopleFill,
+    BsFileCodeFill,
+    BsFillInfoCircleFill,
+    BsPersonFill,
+    BsDiscord,
+    BsStack,
+    BsFillFileSlidesFill,
+  } from "react-icons/bs";
 
 function page() {
   return (
-    <div>
+    <div className="w-[97%] mx-auto">
       <CountdownTimer
         className="bg-transparent border-none text-pink-400"
         eventDate="2024-09-27T17:00:00"
@@ -17,34 +28,188 @@ function page() {
       />
 
       <div className="w-fit mx-auto mb-10">
-        <button className="btn btn-primary mx-auto text-purple-900">Register</button>
+        <button className="btn btn-primary mx-auto text-black font-normal">Register</button>
       </div>
 
       <div className="lg:grid lg:grid-cols-2">
-        <div className="border border-cyan-300 rounded-sm p-4 w-[94%] mx-auto">
-          <div className="text-2xl text-pink-400 font-bold">
-            What is Startup Sprint?
+        <div className="border border-cyan-300 bg-[#F444FF] -400 rounded-md border-2 p-4 w-[94%] mx-auto mt-10">
+          <div className="text-2xl text-base-100 font-bold">
+            💡 What is Startup Sprint?
           </div>
-          <div className="text-lg text-white mt-10">
+
+          <div className="text-xl font-medium text-black mt-4">
             Startup Sprint is a competition spanning multiple days where
-            students will work in teams to build a startup from scratch. This
-            event is open to all students and will be a great opportunity to
+            students will work in teams to build a startup from scratch. 
+            <br /><br />
+            This event is open to all students and will be a great opportunity to
             learn about entrepreneurship and meet other students interested in
-            startups. This event will feature a series of workshops, activities,
+            startups. 
+            <br /><br />
+            Startup Sprint will feature a series of workshops, activities,
             and a chance to win it big.
           </div>
         </div>
 
-        <div className="border border-pink-500 rounded-sm p-4 w-[94%] mx-auto">
-          <div className="text-2xl text-cyan-300 font-bold lg:text-right">
-            Itinerary
+        <div className="border border-pink-500 bg-cyan-300 rounded-md border-2 p-4 w-[94%] mx-auto mt-10">
+            <h2 className="mb-4 text-2xl font-bold text-base-100">
+              🌟 Why Should You Join?
+            </h2>
+
+            <div className="mt-4 text-xl text-black">
+              <div className="font-bold text-purple-900">
+                You'll get hands-on experience:
+              </div>
+              You&apos;re going to be dropped into a fast-paced environment
+              where you&apos;ll have to identify a problem, deploy a solution,
+              and craft a business model.
+            </div>
+            <div className="mt-4 text-xl text-black">
+              <div className="font-bold text-purple-900">
+              You'll level up your skills:
+              </div>
+              Learn to use hyper-modern technologies and apply them to a
+              practical problem.
+            </div>
+            <div className="mt-4 text-xl text-black">
+              <div className="font-bold text-purple-900">Win $$$:</div>
+                Sieze the opportunity to win big cash prizes.
+            </div>
+            <div className="mt-4 text-xl text-black">
+              <div className="font-bold text-purple-900">
+                Boost Your Resume:
+              </div>
+              Show off your ability to work in a team, solve problems, and build solutions.
+            </div>
           </div>
-          <span className="text-2xl text-cyan-300 font-bold">
-            September 27th 5:00 PM at CID
-          </span>
-          <div className="text-lg text-white"></div>
-        </div>
+
+
+
       </div>
+
+      <div className="lg:grid lg:grid-cols-2">
+
+        <div className="border border-pink-500 rounded-sm p-4 w-[94%] mx-auto mt-10 flex flex-col">
+          <div className="text-2xl text-cyan-300 font-bold">
+            What to expect?
+          </div>
+          <div className="text-lg text-white mt-10">
+            Startup Sprint is a challenge to build a startup from scratch in a matter of days.
+            You will have 3-4 days to identify a problem, build a product, and form a business model.
+            Your team will be judged on the quality of your idea, the execution of your product, 
+            and the strength of your business model.
+          </div>
+          <div className="text-lg text-white mt-10">
+            Throughout the course of the event, there will be workshops and resources provided to assist
+            you in developing your startup.
+          </div>
+          <div className="text-lg text-white mt-4">
+            Winners will then be given a <span className="font-bold">lump sum of money </span>
+            You can choose what to do with the prizes you win, whether it be to funnel it into your startup or to split it among your team.
+
+            <div className="mt-4 mb-10">
+                There are also opportunities to connect and get integrated with local incubator programs to further develop your startup.
+            </div>
+
+            <div className="alert alert-success bg-green-400 text-black mt-auto">
+                <FaMoneyBill className="text-2xl"/>
+                Prizes are to be announced soon!
+            </div>
+          </div>
+        </div>
+
+        <div className="border border-cyan-300 rounded-sm p-4 w-[94%] mx-auto mt-10 flex flex-col">
+          <div className="text-2xl text-cyan-300 font-bold lg:text -right mb-10">
+            When is it?
+          </div>
+
+          <div className="text-xl text-cyan-300 font-bold">
+            Opening Ceremony:
+          </div>
+          <div className="font-medium">
+            September 27th
+          </div>
+
+          <div className="text-xl text-cyan-300 font-bold mt-6">
+            Closing Ceremony:
+          </div>
+          <div className="font-medium">
+            October 2nd
+          </div>
+
+          <div className="alert alert-info mt-auto">
+            <BsFillInfoCircleFill className="text-2xl" />
+            Check this page regularly for updates and new information.
+            Location and times will be announced soon.
+          </div>
+        </div>
+
+      </div>
+        <div className="w-full mx-auto mt-10">
+            <div className="">
+                <h2 className="mb-4 text-2xl font-bold text-secondary">FAQs</h2>
+
+                <div className="mb-4 inline-block text-xl">
+                <h3 className="text-2xl font-bold text-primary m">
+                    Can I apply with a startup or product I&apos;m already working
+                    on?
+                </h3>
+                <BsFillExclamationOctagonFill className="mr-4 inline text-2xl text-error" />
+                No previous work may be done before the event. Any evidence of
+                prior work will result in disqualification. You are free to funnel
+                prize money into previous ventures, however. But your submissions
+                must be developed during the event and no earlier.
+                </div>
+
+                <div className="mb-4 inline-block text-xl">
+                <h3 className="text-2xl font-bold text-primary">
+                    Do I need a team to apply?
+                </h3>
+                <BsPeopleFill className="mr-4 inline text-2xl text-error" />
+                You do not need a team to participate, but you are encouraged to
+                form teams of up to 4 people.
+                </div>
+
+                <div className="mb-4 inline-block text-xl">
+                <h3 className="text-2xl font-bold text-primary">
+                    Do I need to know how to code?
+                </h3>
+                <BsFileCodeFill className="mr-4 inline text-2xl text-error" />
+                No, you do not need to know how to code. You must still submit a
+                product, however, and this could simply be a landing page made
+                with Wix and a spreadsheet on your end.
+                </div>
+
+                <div className="mb-4 inline-block text-xl">
+                <h3 className="text-2xl font-bold text-primary">
+                    Who should I contact if I have questions?
+                </h3>
+                <div>
+                    <BsPersonFill className="mr-4 inline text-2xl text-error" />
+                    ebenezer@vt.edu
+                </div>
+                <div>
+                    <BsPersonFill className="mr-4 inline text-2xl text-error" />
+                    htsours@vt.edu
+                </div>
+                <div>
+                    <BsPersonFill className="mr-4 inline text-2xl text-error" />
+                    samuelr20@vt.edu
+                </div>
+                <div>
+                    <BsPersonFill className="mr-4 inline text-2xl text-error" />
+                    mbutler23@vt.edu
+                </div>
+                <div>
+                    <BsPersonFill className="mr-4 inline text-2xl text-error" />
+                    jeremiahhohn@vt.edu
+                </div>
+                <div>
+                    <BsPersonFill className="mr-4 inline text-2xl text-error" />
+                    brodyemery11@vt.edu
+                </div>
+                </div>
+            </div>
+        </div>
     </div>
   );
 }
