dd53f4fda8c1d395a89bfbe73465d98233cf6de8
Ebenezer Zergabachew <eben.zergaw@gmail.com>
2024-08-28 11:30:07 -0400

    newsletter sign up page

---
+++ app/newsletter/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import NewsletterSignup from '../components/NewsletterSignup'
+
+function page() {
+  return (
+    <div className='h-screen'>
+        <h1 className="text-3xl text-primary mx-auto w-fit">Join Our Newsletter</h1>
+        <div className="mx-auto w-1/2 mt-10">
+            <p className='mx-auto text-center'>Stay up to date on all our events and information.</p>
+            <NewsletterSignup></NewsletterSignup>
+        </div>
+    </div>
+  )
+}
+
+export default page
