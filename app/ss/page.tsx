65d7e14c15e36f67e4bf2348fc9ef6d49087abf3
Ebenezer Zergabachew <eben.zergaw@gmail.com>
2024-09-27 16:45:16 -0400

    update

--- app/ss/page.tsx
+++ app/ss/page.tsx
@@ -38,65 +38,253 @@ export default function StartupSprint24Page() {
         alt="Startup Sprint Logo"
       />
 
-      <div className="mx-auto mb-10 w-fit">
-        <a
-          href="https://forms.gle/pFcTMAD8rrgo7ZpeA"
-          target="_blank"
-          className="y btn mx-auto bg-gradient-to-r from-fuchsia-600 to-orange-400 font-semibold text-[#272E3F] hover:text-[#85F3D4] lg:text-xl"
-        >
-          Register
-        </a>
-      </div>
 
       <div className="lg:grid lg:grid-cols-2">
-        <div className="blue-glow mx-auto mt-10 w-[94%] rounded-md border-0 bg-gradient-to-b from-fuchsia-500 to-orange-500 p-4">
-          <div className="text-2xl font-bold text-base-100">
-            💡 What is Startup Sprint?
-          </div>
-
-          <div className="mt-4 text-xl font-medium text-black">
-            Startup Sprint is a competition spanning multiple days where
-            students will work in teams to build a startup from scratch.
-            <br />
-            <br />
-            This event is open to all students and will be a great opportunity
-            to learn about entrepreneurship and meet other students interested
-            in startups.
-            <br />
-            <br />
-            Startup Sprint will feature a series of workshops, activities, and a
-            chance to win it big.
-          </div>
-        </div>
-
-        <div className="blue-glow mx-auto mt-10 w-[94%] rounded-md border-0 bg-gradient-to-b from-fuchsia-500 to-orange-500 p-4">
-          <h2 className="mb-4 text-2xl font-bold text-base-100">
-            🌟 Why Should You Join?
+        <div className="blue-glow mx-auto mt-10 w-[94%] rounded-md border border-purple-600 p-4">
+          <h2 className="text-2xl font-bold text-orange-400 mb-4">
+            🗓️ Important Info
           </h2>
 
-          <div className="mt-4 text-xl text-black">
-            <div className="font-bold text-black">
-              You'll get hands-on experience:
+          <a
+            target="_blank"
+            href="https://eclubvt.notion.site/Startup-Sprint-Rubrics-10ed90daaa0180bcb6effa025f8b280c"
+            className="block text-center font-bold rounded border border-grey-200 py-2 px-4 w-[100%] hover:bg-gray-400"
+          >
+            Rubrics
+          </a>
+
+          <a
+            target="_blank"
+            href={"../../../Startup Sprint 2024.pdf"}
+            className="block text-center font-bold rounded border border-grey-200 py-2 px-4 w-[100%] hover:bg-gray-400 mt-4"
+          >
+            Opening Ceremony Slides
+          </a>
+
+
+          <a
+            target="_blank"
+            href="https://eclubvt.notion.site/Startup-Sprint-Resources-ac1cb75278cc4e8dbdefcce85c9ce2a2"
+            className="block text-center font-bold rounded border border-grey-200 py-2 px-4 w-[100%] hover:bg-gray-400 mt-4"
+          >
+            Resources
+          </a>
+
+          <div>
+            <div className="max-w-md mx-auto my-6 bg- white p-6 rounded-lg shadow-md">
+              <h3 className="text-center font-bold text-2xl">Prizes</h3>
+              <div className="flex justify-between py-2 border-b border-gray-200">
+                <span className="font-semibold">🥇 #1</span>
+                <span className="text-orange-600">$2000</span>
+              </div>
+              <div className="flex justify-between py-2 border-b border-orange-200">
+                <span className="font-semibold">🥈 #2</span>
+                <span className="text-orange-600">$1500</span>
+              </div>
+              <div className="flex justify-between py-2 border-b border-orange-200">
+                <span className="font-semibold">🥉 #3</span>
+                <span className="text-orange-600">$750</span>
+              </div>
+              <div className="flex justify-between py-2 border-b border-orange-200">
+                <span className="font-semibold">🤖 Best AI startup</span>
+                <span className="text-orange-600">$250</span>
+              </div>
+              <div className="flex justify-between py-2 border-b border-orange-200">
+                <span className="font-semibold">💻 Best no code startup</span>
+                <span className="text-orange-600">$250</span>
+              </div>
+              <div className="flex justify-between py-2">
+                <span className="font-semibold">
+                  🌍 Best social impact startup
+                </span>
+                <span className="text-orange-600">$250</span>
+              </div>
             </div>
-            You&apos;re going to be dropped into a fast-paced environment where
-            you&apos;ll have to identify a problem, deploy a solution, and craft
-            a business model.
           </div>
-          <div className="mt-4 text-xl text-black">
-            <div className="font-bold text-black">
-              You'll level up your skills:
-            </div>
-            Learn to use hyper-modern technologies and apply them to a practical
-            problem.
+
+          <div>
+      <div className="max-w-md mx-auto my-6 p-6 rounded-lg shadow-md">
+        <h3 className="text-center font-bold text-2xl">Rules</h3>
+
+        <div className=" py-2 border-b border-gray-200">
+          <div className="font-semibold">No previous work may be submitted</div>
+          <div className="text-white font-bold text-red-500">We will do our due diligence</div>
+        </div>
+        <div className="flex justify-between py-2 pl-6">
+        </div>
+
+        <div className="flex justify-between py-2 border-b border-orange-200">
+          <span className="font-semibold">All participants must be VT students</span>
+        </div>
+
+        <div className="flex justify-between py-2 border-b border-orange-200">
+          <span className="font-semibold">Submission:</span>
+        </div>
+        <div className="flex justify-between py-2 pl-6">
+          <span className="text-red-500 font-bold text-ce ">Due 7:00 AM Tuesday morning</span>
+        </div>
+        <div className="flex justify-between py-2 pl-6">
+          <span className="text-white font-bold text-ce">2 Min Demo video of your product</span>
+        </div>
+        <div className="flex justify-between py-2 pl-6">
+          <span className="text-white font-bold text-ce">Slide deck explaining problem, product, business model</span>
+        </div>
+      </div>
+    </div>
+        </div>
+
+        {/* ITINERARY */}
+        <div className="mx-auto mt-10 w-[94%] flex flex-col rounded-sm border border-pink-500 p-4">
+          <h2 className="text-2xl font-bold text-cyan-300">
+            🗓️ What's the Rundown?
+          </h2>
+          <div className="text-lg text-white mt-4">
+            <p>
+              All events will be held at the{" "}
+              <span className="font-bold">
+                Apex Center (432 North Main St, right above PK's)
+              </span>
+            </p>
           </div>
-          <div className="mt-4 text-xl text-black">
-            <div className="font-bold text-black">Win $$$:</div>
-            Sieze the opportunity to win big cash prizes.
+
+          <div className="mt-6">
+            <h3 className="text-xl font-bold text-cyan-300">
+              Friday, Sep 27 - Kickoff
+            </h3>
+            <p className="text-lg text-white">
+              5:00 PM -{" "}
+              <span className="font-bold">Startup Sprint Kickoff!</span>
+            </p>
+            <p className="text-white">
+              Fireside chat with <span className="font-bold">Kyle Krzeski</span>
+              , CEO of Sabbath Ventures and VT alumnus.
+              <br />
+              Detailed explanation of rules, rubric, and game plan.
+              <br />
+              Team matching session for those looking for teammates.
+              <br />
+              <span className="font-bold">Food provided!</span>
+            </p>
           </div>
-          <div className="mt-4 text-xl text-black">
-            <div className="font-bold text-black">Boost Your Resume:</div>
-            Show off your ability to work in a team, solve problems, and build
-            solutions.
+
+          <div className="mt-6">
+            <h3 className="text-xl font-bold text-cyan-300">
+              Saturday, Sep 28 - Meet Alumni Founders
+            </h3>
+            <p className="text-lg text-white">
+              11:00 AM -{" "}
+              <span className="font-bold">E-Club Recruiting Info Session</span>
+            </p>
+            <p className="text-white">
+              Learn about opportunities to get involved with VT's
+              entrepreneurship ecosystem.
+            </p>
+            <p className="text-lg text-white mt-4">
+              12:00 PM -{" "}
+              <span className="font-bold">Problem Discovery Workshop</span>
+            </p>
+            <p className="text-white">
+              Learn how to form a problem hypothesis based on your unique
+              experiences.
+            </p>
+            <p className="text-lg text-white mt-4">
+              1:30 PM -{" "}
+              <span className="font-bold">
+                Early Startup Market Research and Strategy with AI
+              </span>
+            </p>
+            <p className="text-white">
+              Learn how to analyze markets and build a strategy using modern
+              tools.
+            </p>
+            <p className="text-lg text-white mt-4">
+              2:30 PM -{" "}
+              <span className="font-bold">
+                Fireside Chat with Vince Baranauskas
+              </span>
+            </p>
+            <p className="text-white">
+              Hear from the cofounder and CTO of Macrovation, an advanced
+              materials technology company.
+            </p>
+            <p className="text-lg text-white mt-4">
+              4:30 PM -{" "}
+              <span className="font-bold">
+                Mentorship Sessions with Kyle Krzeski
+              </span>
+            </p>
+            <p className="text-white">
+              Drop in for mentorship and feedback on your project.
+            </p>
+          </div>
+
+          <div className="mt-6">
+            <h3 className="text-xl font-bold text-cyan-300">
+              Sunday, Sep 29 - Meet Student Founders
+            </h3>
+            <p className="text-lg text-white">
+              12:00 PM -{" "}
+              <span className="font-bold">Startup Marketing Workshop</span>
+            </p>
+            <p className="text-white">
+              Learn how to find, target, and acquire customers for your product.
+            </p>
+            <p className="text-lg text-white mt-4">
+              3:00 PM -{" "}
+              <span className="font-bold">
+                Fireside Chat with Mohit Yadav - CTO of QuickPark Solutions
+              </span>
+            </p>
+            <p className="text-white">
+              Hear from a sophomore VT student and cofounder of QuickPark
+              Solutions.
+            </p>
+            <p className="text-lg text-white mt-4">
+              4:30 PM -{" "}
+              <span className="font-bold">
+                Meet the Team of Exit Sign Solutions
+              </span>
+            </p>
+            <p className="text-white">
+              Exit Sign Solutions is a startup building unbreakable exit signs
+              and has raised $85,000 from investors.
+            </p>
+          </div>
+
+          <div className="mt-6">
+            <h3 className="text-xl font-bold text-cyan-300">Monday, Sep 30</h3>
+            <p className="text-lg text-white">
+              10:00 AM - 2:00 PM -{" "}
+              <span className="font-bold">Office Hours with Kyle Krzeski</span>
+            </p>
+            <p className="text-white">
+              Come for last-minute advice or help before the submission
+              deadline.
+            </p>
+            <p className="text-lg text-white mt-4">
+              11:59 PM - <span className="font-bold">Submissions Due</span>
+            </p>
+          </div>
+
+          <div className="mt-6">
+            <h3 className="text-xl font-bold text-cyan-300">Tuesday, Oct 1</h3>
+            <p className="text-white">Take a break—you deserve it! 😊</p>
+          </div>
+
+          <div className="mt-6">
+            <h3 className="text-xl font-bold text-cyan-300">
+              Wednesday, Oct 2 - Closing Ceremony
+            </h3>
+            <p className="text-lg text-white">
+              6:00 PM -{" "}
+              <span className="font-bold">
+                Finalist Announcements & Pitches
+              </span>
+            </p>
+            <p className="text-white">
+              The finalists will pitch their startups for the chance to win big
+              $$$.
+            </p>
           </div>
         </div>
       </div>
@@ -133,6 +321,7 @@ export default function StartupSprint24Page() {
           </div>
         </div>
 
+        {/* ITINERARY */}
         <div className="mx-auto mt-10 w-[94%] flex flex-col rounded-sm border border-pink-500 p-4">
           <h2 className="text-2xl font-bold text-cyan-300">
             🗓️ What's the Rundown?
@@ -145,8 +334,6 @@ export default function StartupSprint24Page() {
               </span>
             </p>
           </div>
-
-          
 
           <div className="mt-6">
             <h3 className="text-xl font-bold text-cyan-300">
