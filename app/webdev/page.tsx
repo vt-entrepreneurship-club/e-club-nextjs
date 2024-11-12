fb2c5c050729858430f676d7969dd5f5119902c3
eclubvt <144861703+eclubvt@users.noreply.github.com>
2024-11-11 20:10:52 -0500

    Update page.tsx

--- app/webdev/page.tsx
+++ app/webdev/page.tsx
@@ -5,6 +5,7 @@ export default function page() {
     <div className='flex flex-col items-center h-screen'>
       <h1 className='text-4xl font-bold'>Web Development Workshop</h1>
 
+      
       <button className='bg-primary text-white px-4 py-2 rounded-md mt-20'>
         <a href="https://eclubvt.notion.site/Web-Dev-Public-Page-13bd90daaa0180109165cf1d36b02be0">
           <p>Click here</p>
