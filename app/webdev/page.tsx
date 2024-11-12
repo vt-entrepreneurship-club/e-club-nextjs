4abdf5a91ae4a05c111dbf8490fcc130b2dae283
Ebenezer Zergabachew <eben.zergaw@gmail.com>
2024-11-11 20:01:40 -0500

    webdev page

---
+++ app/webdev/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='flex flex-col items-center h-screen'>
+      <h1 className='text-4xl font-bold'>Web Development Workshop</h1>
+
+      <button className='bg-primary text-white px-4 py-2 rounded-md mt-20'>
+        <a href="https://eclubvt.notion.site/Web-Dev-Public-Page-13bd90daaa0180109165cf1d36b02be0">
+          <p>Click here</p>
+        </a>
+      </button>
+    </div>
+  )
+}
+
